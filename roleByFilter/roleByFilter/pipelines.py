9242a22e4c1b57073c999b20b8421df05ac795f9
王雄 <6874024@qq.com>
2021-11-26 11:22:31 +0800

    feat: 数据库调整

--- roleByFilter/roleByFilter/pipelines.py
+++ roleByFilter/roleByFilter/pipelines.py
@@ -15,7 +15,7 @@ from itemadapter import ItemAdapter
 class RolebyfilterPipeline:
     def __init__(self):
         # 连接数据库
-        self.connect=pymysql.connect(host='localhost',user='root',password='root',db='rundstedt')
+        self.connect=pymysql.connect(host='localhost',user='root',password='rundstedt2021',db='rundstedt')
         self.cursor=self.connect.cursor()
         # 重置记录 txt
         startTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
@@ -49,23 +49,23 @@ class RolebyfilterPipeline:
 
     def process_item(self, item, spider):
         if item['itemType'] == 'role' :
-            sqlStatement = "insert into rundstedt.role (id, name, gender, school, neigongyanxiu, price, status, server_name,server_id,grade)VALUES ({},'{}','{}','{}','{}',{},'{}','{}',{},'{}')"
+            sqlStatement = "insert into rundstedt.role (id, name, gender, school, neigongyanxiu, price, status, server_name,server_id,grade)VALUES ('{}','{}','{}','{}','{}',{},'{}','{}','{}','{}')"
             self.cursor.execute(sqlStatement.format(item['roleID'],item['name'],item['gender'],item['school'],item['neigongyanxiu'],item['price'],item['status'],item['server'],item['serverId'],item['grade']))
             self.connect.commit()#执行添加
         elif item['itemType'] == 'treasure' :
-            sqlStatement = "insert into rundstedt.treasure (id, role_id, data_info, wuxue, is750)VALUES ({},{},'{}','{}',{})"
+            sqlStatement = "insert into rundstedt.treasure (id, role_id, data_info, wuxue, is750)VALUES ('{}','{}','{}','{}',{})"
             self.cursor.execute(sqlStatement.format(item['tID'], item['roleID'],escape_string(item['dataInfo']),item['skill'],item['is750']))
             self.connect.commit()#执行添加
         elif item['itemType'] == 'treasureprop' :
-            sqlStatement = "insert into rundstedt.treasure_prop (treasure_id, prop) SELECT {},'{}' FROM DUAL WHERE NOT EXISTS (SELECT * FROM rundstedt.treasure_prop WHERE treasure_id= {} AND prop='{}')"
+            sqlStatement = "insert into rundstedt.treasure_prop (treasure_id, prop) SELECT '{}','{}' FROM DUAL WHERE NOT EXISTS (SELECT * FROM rundstedt.treasure_prop WHERE treasure_id= '{}' AND prop='{}')"
             self.cursor.execute(sqlStatement.format(item['tID'],item['prop'],item['tID'],item['prop']))
             self.connect.commit()#执行添加
         elif item['itemType'] == 'threeSkills' :
-            sqlStatement = "insert into rundstedt.threeskills (role_id, data_info, wuxue, skill)VALUES ({},'{}','{}','{}')"
+            sqlStatement = "insert into rundstedt.threeskills (role_id, data_info, wuxue, skill)VALUES ('{}','{}','{}','{}')"
             self.cursor.execute(sqlStatement.format(item['roleID'],escape_string(item['dataInfo']),item['wuxue'],item['skill']))
             self.connect.commit()#执行添加
         elif item['itemType'] == 'skin' :
-            sqlStatement = "insert into rundstedt.skin (role_id, name, type, quality, photo)VALUES ({},'{}','{}','{}','{}')"
+            sqlStatement = "insert into rundstedt.skin (role_id, name, type, quality, photo)VALUES ('{}','{}','{}','{}','{}')"
             self.cursor.execute(sqlStatement.format(item['roleID'],item['name'],item['type'],item['quality'],item['photo']))
             self.connect.commit()#执行添加
         return item
